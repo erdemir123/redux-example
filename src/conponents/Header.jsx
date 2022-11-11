a2aecf8f0e1dff877db42779c1222ee78c321f9f
erdemir123 <sadikerdemir93@gmail.com>
2022-11-12 02:43:08 +0300

    feat:Store designed

--- src/conponents/Header.jsx
+++ src/conponents/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     const state = useSelector((state)=>state)
   return (
     <div className=' fixed top-0 w-full flex h-10 bg-emerald-300 text-orange-900 font-bold uppercase justify-between items-center px-4'>
-      <div>LOGO</div>
+      <div>Pars Bilişim</div>
       <div className='flex gap-3 '>
         <Link to={"/"}> Store</Link>
         <Link to={"/card"}> Card{state.length ? `(${state.length})` : ""}</Link>
